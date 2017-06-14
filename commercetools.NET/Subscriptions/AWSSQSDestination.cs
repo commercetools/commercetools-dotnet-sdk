bdf74825d5863fb02d57f33fd813087057189209
jhumber <justinhumber@gmail.com>
2017-06-13 20:16:57 -0700

    Added representations for Subscriptions endpoint

---
+++ commercetools.NET/Subscriptions/AWSSQSDestination.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+
+namespace commercetools.Subscriptions
+{
+    /// <summary>
+    /// AWS SQS is a pull-queue on AWS.
+    /// </summary>
+    /// <remarks>
+    /// The queue needs to exist beforehand. It is recommended to create an accessKey and accessSecret pair specifically for each subscription that only has the sqs:SendMessage permission on this queue.
+    /// </remarks>
+    /// <see href="https://dev.commercetools.com/http-api-projects-subscriptions.html#aws-sqs-destination"/>
+    /// <seealso href="https://aws.amazon.com/sqs/"/>
+    public class AWSSQSDestination : Destination
+    {
+        #region Properties
+
+        /// <summary>
+        /// QueueUrl
+        /// </summary>
+        [JsonProperty(PropertyName = "queueURL")]
+        public string QueueUrl { get; private set; }
+
+        /// <summary>
+        /// AccessKey
+        /// </summary>
+        [JsonProperty(PropertyName = "accessKey")]
+        public string AccessKey { get; private set; }
+
+        /// <summary>
+        /// AccessSecret
+        /// </summary>
+        [JsonProperty(PropertyName = "accessSecret")]
+        public string AccessSecret { get; private set; }
+
+        /// <summary>
+        /// Region
+        /// </summary>
+        [JsonProperty(PropertyName = "region")]
+        public string Region { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes this instance with JSON data from an API response.
+        /// </summary>
+        /// <param name="data">JSON object</param>
+        public AWSSQSDestination(dynamic data)
+            : base((object)data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            this.QueueUrl = data.queueURL;
+            this.AccessKey = data.accessKey;
+            this.AccessSecret = data.accessSecret;
+            this.Region = data.region;
+        }
+
+        #endregion
+    }
+}
---
+++ commercetools.NET/Subscriptions/ChangeSubscription.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace commercetools.Subscriptions
+{
+    /// <summary>
+    /// ChangeSubscription
+    /// </summary>
+    /// <remarks>
+    /// Different payloads for resource creation, update and deletion are delivered.
+    /// </remarks>
+    /// <see href="https://dev.commercetools.com/http-api-projects-subscriptions.html#changesubscription"/>
+    public class ChangeSubscription 
+    {
+        #region Properties
+
+        /// <summary>
+        /// ResourceTypeId
+        /// </summary>
+        [JsonProperty(PropertyName = "resourceTypeId")]
+        public string ResourceTypeId { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes this instance with JSON data from an API response.
+        /// </summary>
+        /// <param name="data">JSON object</param>
+        public ChangeSubscription(dynamic data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            this.ResourceTypeId = data.resourceTypeId;
+        }
+
+        #endregion
+    }
+}
