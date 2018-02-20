d19011c8c1ce97f6a207f624b38a5f2e392d9161
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2018-02-20 11:43:20 +0100

    WATSON-1229 Add SetKey to customer update actions

---
+++ src/commercetools.Core/Customers/UpdateActions/SetKeyAction.cs
@@ -0,0 +1,34 @@
+﻿using commercetools.Core.Common;
+using Newtonsoft.Json;
+
+namespace commercetools.Core.Customers.UpdateActions
+{
+    /// <summary>
+    /// Set key
+    /// </summary>
+    /// <see href="https://docs.commercetools.com/http-api-projects-customers.html#set-key"/>
+    public class SetKeyAction : UpdateAction
+    {
+        #region Properties
+
+        /// <summary>
+        /// Customer key
+        /// </summary>
+        [JsonProperty(PropertyName = "key")]
+        public string Key { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SetKeyAction()
+        {
+            this.Action = "setKey";
+        }
+
+        #endregion
+    }
+}
