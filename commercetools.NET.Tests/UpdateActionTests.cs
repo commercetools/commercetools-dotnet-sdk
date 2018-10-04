69153ebd2af5c16ed04252dd44519f7667644b70
Ben Konsemüller <bkonsem@gmail.com>
2018-10-04 09:24:50 +0200

    SetAttributeAction Staged property defaults to true now (#77)
    * SetAttributeAction Staged property defaults to true now
    
    * Added test for SetAttributeAction

---
+++ commercetools.NET.Tests/UpdateActionTests.cs
@@ -0,0 +1,26 @@
+﻿using commercetools.Products.UpdateActions;
+using NUnit.Framework;
+
+namespace commercetools.Tests
+{
+    /// <summary>
+    /// Tests the Update Actions
+    /// </summary>
+    [TestFixture]
+    public class UpdateActionTests
+    {
+        /// <summary>
+        /// Tests whether the default value of the property Staged by SetAttributeAction is set to true.
+        /// <see cref="SetAttributeAction.Staged"/>
+        /// </summary>
+        [Test]
+        public void SetAttributeActionStagedDefaultsToTrue()
+        {
+            var actionName = "Testaction";
+            var variantId = 1;
+
+            var setAttributeAction = new SetAttributeAction(actionName, variantId);
+            Assert.True(setAttributeAction.Staged);
+        }
+    }
+}
