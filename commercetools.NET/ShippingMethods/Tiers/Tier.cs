d01e5f1c1c38d7984192972f263386a7126bcffd
Ben Konsemüller <bkonsem@gmail.com>
2018-10-04 18:53:10 +0200

    Moved and Tier class to be abstract

---
+++ commercetools.NET/ShippingMethods/Tiers/Tier.cs
@@ -0,0 +1,22 @@
+﻿using commercetools.Common;
+using Newtonsoft.Json;
+
+namespace commercetools.ShippingMethods.Tiers
+{
+    /// <summary>
+    /// The representation to be sent to the server when creating a new shipping rate price tier.
+    /// </summary>
+    /// <see href="https://docs.commercetools.com/http-api-projects-shippingMethods.html#shippingratepricetier"/>
+    public abstract class Tier
+    {
+        #region Properties
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; protected set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public Money Price { get; protected set; }
+
+        #endregion
+    }
+}
